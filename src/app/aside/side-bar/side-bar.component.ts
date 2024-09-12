e7c2a589bbd6c39655ef8a1a3390a2df05ad60e7
Paulo <pshenrique2015@gmail.com>
2024-09-11 22:10:40 -0300

    add: basic components

--- src/app/aside/side-bar/side-bar.component.ts
+++ src/app/aside/side-bar/side-bar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-side-bar',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule],
   templateUrl: './side-bar.component.html',
   styleUrl: './side-bar.component.scss'
 })
