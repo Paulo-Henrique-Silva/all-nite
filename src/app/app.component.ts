7f52770c9155d19216eb8a0d8dbd030d81d5a275
Paulo <pshenrique2015@gmail.com>
2024-09-14 13:00:00 -0300

    fix: minor changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { SideBarComponent } from "./aside/side-bar/side-bar.component";
 import { MapComponent } from "./map/map.component";
 import { DividerModule } from 'primeng/divider';
+import { AnimateModule } from 'primeng/animate';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, SideBarComponent, MapComponent, DividerModule],
+  imports: [CommonModule, RouterOutlet, SideBarComponent, MapComponent, DividerModule, AnimateModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
