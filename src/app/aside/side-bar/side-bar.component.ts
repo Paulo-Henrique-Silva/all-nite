730b383c09da4adb5ac2c47ddb7241c3202326f6
Paulo <pshenrique2015@gmail.com>
2024-09-13 23:01:44 -0300

    ADD: navbar

--- src/app/aside/side-bar/side-bar.component.ts
+++ src/app/aside/side-bar/side-bar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { DividerModule } from 'primeng/divider';
 
 @Component({
   selector: 'app-side-bar',
   standalone: true,
-  imports: [ButtonModule],
+  imports: [ButtonModule, DividerModule],
   templateUrl: './side-bar.component.html',
   styleUrl: './side-bar.component.scss'
 })
