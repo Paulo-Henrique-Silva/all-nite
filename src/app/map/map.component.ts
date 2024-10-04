bea446322218d39662c04115adc90a9f5f8dc91e
Paulo <pshenrique2015@gmail.com>
2024-10-03 22:48:45 -0300

    ADD: Added map click event handler and marker array to map component

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { AngularOpenlayersModule } from "ng-openlayers";
 import { MapService } from '../shared/services/map.service';
 import { AntLocation } from '../shared/models/ant-location';
+import { fromLonLat, toLonLat } from 'ol/proj';
+//import { AolMarkerModule } from 'ngx-ol-library';
+
 
 @Component({
   selector: 'app-map',
@@ -15,6 +18,22 @@ export class MapComponent implements OnInit {
 
   constructor(mapService: MapService) {
     this.mapService = mapService.curlocation;
+  }
+
+  markers: { x: number, y: number }[] = [];
+
+  // Method to handle map click event
+  onMapClick(event: any): void {
+    const map = event.map; // The OpenLayers map object
+
+    // Get the clicked coordinates
+    const clickedCoordinate = map.getEventCoordinate(event.originalEvent);
+
+    // Convert from map projection to 'EPSG:4326' (lat/lon)
+    const lonLat = toLonLat(clickedCoordinate);
+
+    // Push the marker into the array
+    this.markers.push({ x: lonLat[0], y: lonLat[1] });
   }
 
   ngOnInit(): void {
