23ae51f83c80c788643996985ec6794c7ac24ed9
Paulo <pshenrique2015@gmail.com>
2024-11-11 22:30:59 -0300

    ADD: Delete events function

--- src/app/aside/search/search.component.ts
+++ src/app/aside/search/search.component.ts
@@ -1,19 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AntEvent } from '../../shared/models/ant-event';
-import { AntLocation } from '../../shared/models/ant-location';
 import { CommonModule } from '@angular/common';
 import { CardModule } from 'primeng/card';
 import { ScrollPanelModule } from 'primeng/scrollpanel';
 import { MapService } from '../../map/services/map.service';
 import { InputTextModule } from 'primeng/inputtext';
 import { StorageService } from '../../shared/services/storage.service';
-
+import { ButtonModule } from 'primeng/button';
+import { TooltipModule } from 'primeng/tooltip';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [FormsModule, CommonModule, CardModule, ScrollPanelModule, InputTextModule],
+  imports: [FormsModule, CommonModule, CardModule, ScrollPanelModule, InputTextModule, ButtonModule, TooltipModule],
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss'
 })
@@ -36,13 +36,13 @@ export class SearchComponent implements OnInit {
   }
   
   ngOnInit(): void {
-    this.filteredEvents = this.events.sort((a, b) => a.name.localeCompare(b.name))
+    this.filteredEvents = this.events.sort((a, b) => a.name.localeCompare(b.name));
   }
   
   filter(): void {
     if (this.eventNameQuery.length != 0) {
-      this.filteredEvents = this.events.filter(e => e.name.toUpperCase().includes(this.eventNameQuery.toUpperCase()))
-      return
+      this.filteredEvents = this.events.filter(e => e.name.toUpperCase().includes(this.eventNameQuery.toUpperCase()));
+      return;
     }
 
     this.filteredEvents = this.events
@@ -56,5 +56,11 @@ export class SearchComponent implements OnInit {
     this.mapService.makerLocation.cordinateY = antEvent.location.cordinateY;
 
     this.mapService.isEventLocationSet = true;
+  }
+
+  deleteEvent(id: string): void {
+    this.storageService.removeItem(id);
+    this.events = this.events.filter(e => e.id != id);
+    this.filteredEvents = this.filteredEvents.filter(e => e.id != id);;
   }
 }
